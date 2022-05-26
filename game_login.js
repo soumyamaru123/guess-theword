6c11b6bf9aa693947191c8fc418a202539b15bde
soumyamaru123 <99595393+soumyamaru123@users.noreply.github.com>
2022-05-26 14:59:52 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function adduser(){
+    player1_name = document.getElementById("player_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+        localStorage.getItem("player1_name",player1_name);
+        localStorage.getItem("player2_name",player2_name);
+
+         window.location= "game_page.html";
+
+}
